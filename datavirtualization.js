d5171942a86acb80cb0fd27536225ac238886e85
kirkzhang49 <314698267@qq.com>
2020-07-15 16:13:07 -0700

    a

--- datavirtualization.js
+++ datavirtualization.js
@@ -3,8 +3,6 @@
     const gdpData = await d3.csv('https://raw.githubusercontent.com/kirkzhang49/datavirtualization/master/gdp_by_state.csv');
     const UnitedStateIncome = incomeData[0];
     const UnitedStateGdb = gdpData[0];
-    const population = await d3.csv("https://raw.githubusercontent.com/kirkzhang49/datavirtualization/master/population.csv");
-    const names = await d3.json('https://s3-us-west-2.amazonaws.com/vida-public/geo/us.json');
     //1997-2015
     const phase = [1997,1998,1999,2000,2001,2002,2003,2004,2005,2006,2007,2008,2009,2010,2011,2012,2013,2014,2015];
     function generateIncomePhase(data, gdpData) {
