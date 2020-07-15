00a9bab08680405d667bac3cd7542a4d197bf7bd
kirkzhang49 <314698267@qq.com>
2020-07-14 20:06:00 -0700

    basic graph

--- datavirtualization.js
+++ datavirtualization.js
@@ -1,1 +1,81 @@
-d3.csv('gdp_by_state.csv',(data)=>console.log(data))
+(async function() {
+    const incomeData = await d3.csv('https://raw.githubusercontent.com/kirkzhang49/datavirtualization/master/meidum_income.csv');
+    const gdpData = await d3.csv('https://raw.githubusercontent.com/kirkzhang49/datavirtualization/master/gdp_by_state.csv');
+    const UnitedStateIncome = incomeData[0];
+    const UnitedStateGdb = gdpData[0];
+    //1997-2015
+    const phase = [1997,1998,1999,2000,2001,2002,2003,2004,2005,2006,2007,2008,2009,2010,2011,2012,2013,2014,2015];
+    function generateIncomePhase(data, gdpData) {
+        const result = new Array(phase.length);
+        for (let i=0;i<phase.length;i++) {
+            // if (!result['phase']) result['phase'] = new Array(phase.length);
+            result[i] = {
+                'year': phase[i],
+                'income':  Number(data[phase[i]].split(',').join('')) / 1.852,
+                'gdp': Number(gdpData[phase[i]].split(',').join('')),
+            }
+        }
+        return result;
+    }
+    const USPhase = generateIncomePhase(UnitedStateIncome, UnitedStateGdb);
+    console.log(USPhase);
+
+    function generateBarGraph(data,value,color,xx,yy,texts,labelx,labely) {
+        var svg = d3.select("svg"),
+        width = 600,
+        height = 400;
+        var xScale = d3.scaleBand().range([0, width]).padding(0.4),
+            yScale = d3.scaleLinear().range([height, 0]);
+
+        var g = svg.append("g")
+            .attr("transform", "translate(" + xx + "," + yy + ")");
+            xScale.domain(data.map((d)=>d.year));
+            yScale.domain([0, d3.max(data,(d)=>d[value])]);
+            g.append("g")
+            .attr("transform", "translate(0," + height + ")")
+            .call(d3.axisBottom(xScale))
+            .append("text")
+            .attr('id', `${value}-x`)
+            .attr("y", height - 375)
+            .attr("x", width - 100)
+            .attr("text-anchor", "end")
+            .attr("stroke", "black")
+            .text("Year");
+
+            g.append("g")
+            .call(d3.axisLeft(yScale).tickFormat(function(d){
+                return "$" + d;
+            }).ticks(10))
+            .append("text")
+            .attr('id', `${value}-y`)
+            .attr("y", 6)
+            .attr("dy", "0.71em")
+            .attr("text-anchor", "end")
+            .attr("stroke", "black")
+            .text("value").text("Dollars");
+
+             svg.append("text")
+            .attr('id', `${value}-txt`)
+            .attr("transform", `translate(${labelx},${labely})`)
+            .attr("x", 50)
+            .attr("y", 50)
+            .attr("font-size", "24px")
+            .text(texts);
+
+        g.selectAll("rect")
+         .data(data)
+         .enter().append("rect")
+         .attr('class', value)
+         .style("fill", color)
+         .attr("x", function(d) { return xScale(d.year); })
+         .attr("y", function(d) { return yScale(d[value]); })
+         .attr("height", function(d) { return height - yScale(d[value]); })
+         .transition()
+         .duration(1200)
+         .attr("width", xScale.bandwidth())
+    }
+    generateBarGraph(USPhase,'income', '#69b3a2',100,120,'Medium Income chart', 230,20);
+    generateBarGraph(USPhase,'gdp', 'steelblue',800,120, 'GDP Per Person', 990,20);
+    // d3.selectAll(`rect.income`).remove();
+    // d3.select('#income-txt').remove();
+})();
