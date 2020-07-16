10c088b1bdc7667c627c6a75e9f9a323c6f26cd3
kirkzhang49 <314698267@qq.com>
2020-07-15 17:17:52 -0700

    csv

--- datavirtualization.js
+++ datavirtualization.js
@@ -133,8 +133,8 @@ var promises = [
   d3.tsv("https://raw.githubusercontent.com/kirkzhang49/datavirtualization/master/us_map.tsv", function(d) {
     stateNames.set(d.id, d.name)
   }),
-  d3.tsv("https://raw.githubusercontent.com/kirkzhang49/datavirtualization/master/map.tsv", function(d) { 
-    unemployment.set(d.name, +d.value); 
+  d3.csv("https://raw.githubusercontent.com/kirkzhang49/datavirtualization/master/map.tsv", function(d) { 
+    unemployment = d;
   })
 ]
 Promise.all(promises).then(ready)
